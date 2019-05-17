90722945ef52a616e24f40659bc64d89f154b894
David Mogbeyi <mogbeyidavid@gmail.com>
2019-05-17 22:29:15 +0100

    added a failing testcase

--- test/src/add.test.js
+++ test/src/add.test.js
@@ -6,6 +6,6 @@ describe('ADD', () => {
     expect(add(10, 10)).toEqual(20);
   })
   it('should add two numbers', () => {
-    expect(add(10, 5)).toEqual(15);
+    expect(add(10, 5)).toEqual(5);
   })
 });
